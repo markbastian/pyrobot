ab0e62ac7a27586f85dfd438363db1d677392da2
Mark Bastian <markbastian@gmail.com>
2019-09-14 10:14:45 -0600

    incremental changes

--- bot2.py
+++ bot2.py
@@ -4,6 +4,8 @@ from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
 from ev3dev2.sensor.lego import TouchSensor, ColorSensor, UltrasonicSensor, GyroSensor
 from ev3dev2.led import Leds
 from ev3dev2.sound import Sound
+from timeit import default_timer as timer
+import csv
 
 # Universal
 sound = Sound()
@@ -32,7 +34,6 @@ def test_touch():
     while touch.is_pressed:
         leds.set_color("LEFT", "RED")
         leds.set_color("RIGHT", "RED")
-
     leds.set_color("LEFT", "GREEN")
     leds.set_color("RIGHT", "GREEN")
 
@@ -59,27 +60,104 @@ def reset_gyro():
         gyro.mode = mode
 
 
-def balance(k_p=1.0, k_i=0.0, k_d=0.0, baseline=1.0):
+# balance(k_p=1.2, k_i=16.0, k_d=0.0, cum_error=-0.25)
+# Working around this, am I even close?
+# balance(k_p=1.0, k_i=0.5, k_d=0.01, cum_error=0.0, max_theta=45)
+# balance(k_p=0.5, k_i=1.0, k_d=0.01, cum_error=0.0, max_theta=45) - doesn't freak out very much
+# Oscillates a little bit
+# balance(k_p=0.8, k_i=0.0, k_d=0.01, cum_error=0.0, max_theta=45)
+def balance(k_p=1.2, k_i=16.0, k_d=0.02, cum_error=-0.25, max_theta=45):
     reset_gyro()
-    max_theta = 30
-    # A basic drop test shows a max rate of about 493
-    max_rate = 500
     integrated_error = 0.0
+    t = timer()
     theta, rate = gyro.angle_and_rate
-    while abs(theta) <= max_theta:
-        error = constraint_to_unit((theta - baseline) / max_theta)
-        rate_error = constraint_to_unit(rate / max_rate)
-        pid = k_p * error + k_i * integrated_error + k_d * rate_error
-        speedpct = 100.0 * constraint_to_unit(pid)
-        print('theta=%s, rate=%s, error=%s, rate_error=%s, pid=%s, speedpct=%s' %
-              (theta, rate, error, rate_error, pid, speedpct))
-        if pid != 0:
-            speed = SpeedPercent(speedpct)
-            tank_drive.on(speed, speed)
-        else:
-            tank_drive.off()
-        theta, rate = gyro.angle_and_rate
-    tank_drive.off()
+    d_rate = 0.0
+    recording = False
+    with open('P{}_I{}_D{}.csv'.format(k_p, k_i, k_d), 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['t', 'angle', 'rate', 'd_rate', 'i_rate', 'P', 'I', 'D', 'pid', 'speedpct'])
+        while abs(theta) <= max_theta:
+            error = rate
+            rate_error = d_rate
+            integrated_error = cum_error
+            P = k_p * error
+            I = k_i * integrated_error
+            D = k_d * rate_error
+            pid = P + I + D
+            speedpct = max(min(pid, 100.0), -100.0)
+            if rate_error != 0:
+                recording = True
+            if recording:
+                writer.writerow([t, theta, rate, rate_error, integrated_error, P, I, D, pid, speedpct])
+            # print('theta=%s, rate=%s, error=%s, rate_error=%s, pid=%s, speedpct=%s' %
+            #       (theta, rate, error, rate_error, pid, speedpct))
+            print('theta=%s, rate=%s, error=%s, rate_error=%s, cum_error=%s, pid=%s, speedpct=%s' %
+                  (theta, rate, error, rate_error, integrated_error, pid, speedpct))
+            if speedpct != 0:
+                speed = SpeedPercent(speedpct)
+                tank_drive.on(speed, speed)
+            else:
+                tank_drive.off()
+            t_old = t
+            rate_old = rate
+            t = timer()
+            dt = t - t_old
+            theta, rate = gyro.angle_and_rate
+            d_rate = (rate - rate_old) / dt
+            cum_error += rate * dt
+        tank_drive.off()
+
+# def balance_j():
+#     reset_gyro()
+#     max_theta = 30
+#     t = timer()
+#     mPos = 0
+#     loop_count = 0
+#     ready = False
+#     theta, rate = gyro.angle_and_rate
+#     cum_error = -0.25
+#     m = [0, 0, 0, 0]
+#     while abs(theta) <= max_theta:
+#         t_old = t
+#         t = timer()
+#         dt = t - t_old
+#         theta, rate = gyro.angle_and_rate
+#         cum_error = cum_error + rate * dt
+#         curr_speed = left_motor.speed + right_motor.speed - m[0]
+#         m = ([curr_speed] + m)[:4]
+#         print(m)
+#         mSpd = sum(m) / 4.0 / dt
+#         pwr = 0.08 * mSpd + 0.12 * m[0] + 0.8 * rate + 15 * cum_error
+#         pwr = min(max(pwr, -100), 100)
+#         if ready:
+#             s = SpeedPercent(pwr)
+#             tank_drive.on(s, s)
+#         loop_count += 1
+#         if loop_count == 10:
+#             ready = True
+#     tank_drive.off()
+
+# def angle_balance(k_p=1.0, k_i=0.0, k_d=0.0, baseline=1.0):
+#     reset_gyro()
+#     max_theta = 30
+#     # A basic drop test shows a max rate of about 493
+#     max_rate = 500
+#     integrated_error = 0.0
+#     theta, rate = gyro.angle_and_rate
+#     while abs(theta) <= max_theta:
+#         error = constraint_to_unit((theta - baseline) / max_theta)
+#         rate_error = constraint_to_unit(rate / max_rate)
+#         pid = k_p * error + k_i * integrated_error + k_d * rate_error
+#         speedpct = 100.0 * constraint_to_unit(pid)
+#         print('theta=%s, rate=%s, error=%s, rate_error=%s, pid=%s, speedpct=%s' %
+#               (theta, rate, error, rate_error, pid, speedpct))
+#         if pid != 0:
+#             speed = SpeedPercent(speedpct)
+#             tank_drive.on(speed, speed)
+#         else:
+#             tank_drive.off()
+#         theta, rate = gyro.angle_and_rate
+#     tank_drive.off()
 
 # balance(k_p=5.0, k_i=0.0, k_d=0.0)
 # balance(k_p=4.0, k_i=0.0, k_d=0.0)
--- reporting.py
+++ reporting.py
@@ -1,12 +1,27 @@
 import socket
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind(('192.168.0.24', 8089))
-serversocket.listen(5) # become a server socket, maximum 5 connections
+# iMac
+serversocket.bind(('192.168.0.9', 8089))
+# bot
+# serversocket.bind(('192.168.0.17', 8089))
+# become a server socket, maximum 5 connections
+serversocket.listen(5)
+
+connection, address = serversocket.accept()
 
 while True:
-    connection, address = serversocket.accept()
     buf = connection.recv(64)
     if len(buf) > 0:
-        print(buf)
-        break
+        msg = buf.decode('UTF-8')
+        if msg == 'EOF':
+            break
+        else:
+            print(msg)
+
+
+# Client - Add code to send state info to the server to be plotted.
+def doclient():
+    clientsocket = socket.socket()
+    clientsocket.connect(('192.168.0.9', 8089))
+    clientsocket.send('This is a test'.encode())
